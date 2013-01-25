01b5450ba1b94e0c19f48216c0a1ba8d496d95bc
Kimmo Keskinen <keskinenkimmo@gmail.com>
2013-01-26 01:00:02 +0200

    Adding gamewank.js

---
+++ gamewank.js
@@ -0,0 +1,22 @@
+var GameWank = function() {
+    this.isAnExample = false;
+};
+
+Game.prototype.draw = function(canvas, ctx) {
+};
+
+Game.prototype.update = function(timeDelta) {
+};
+
+Game.prototype.mousedown = function(event) {
+	this.clickHeld = true;
+};
+
+Game.prototype.mousemove = function(event) {
+};
+
+Game.prototype.mouseup = function(event) {
+};
+
+Game.prototype.load = function() {
+};
