7956a3612ee304ffdae92f45c96008d5a93f80f2
Olli Etuaho <olli.etuaho@gmail.com>
2013-01-26 00:49:29 +0200

    Add game skeleton implementing game interface

---
+++ game.js
@@ -0,0 +1,18 @@
+var Game = function() {
+    this.isAnExample = true;
+};
+
+Game.prototype.draw = function(ctx) {
+};
+
+Game.prototype.update = function(timeDelta) {
+};
+
+Game.prototype.mousedown = function(event) {
+};
+
+Game.prototype.mousemove = function(event) {
+};
+
+Game.prototype.mouseup = function(event) {
+};
