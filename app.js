468906fa511f1651d8cea023f00744c1508257ca
Olli Etuaho <olli.etuaho@gmail.com>
2013-01-25 22:56:58 +0200

    Flesh the skeleton out a bit

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ var ctx;
 
 function init() {
     canvas = document.createElement('canvas');
+    canvas.id = 'game';
+    canvas.width = 640;
+    canvas.height = 480;
     ctx = canvas.getContext('2d');
     document.body.appendChild(canvas);
-    ctx.fillRect(0, 0, 10, 10);
+    ctx.fillStyle = '#f9c';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
