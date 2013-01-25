21c32c910f80b0b905e64743eda3f02ae0af4088
Olli Etuaho <olli.etuaho@gmail.com>
2013-01-25 23:07:40 +0200

    Frames!

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 var canvas;
 var ctx;
 
+(function() {
+  var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
+                              window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+  window.requestAnimationFrame = requestAnimationFrame;
+})();
+
+var start = Date.now(); 
+
+function doFrame() {
+    var timestamp = Date.now(); 
+    var progress = timestamp - start;
+    ctx.fillStyle = 'rgb(' + Math.round(Math.sin(progress * 0.001) * 128 + 128) + ',0, 0)';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    requestAnimationFrame(doFrame);
+}
+
 function init() {
     canvas = document.createElement('canvas');
     canvas.id = 'game';
@@ -10,4 +26,5 @@ function init() {
     document.body.appendChild(canvas);
     ctx.fillStyle = '#f9c';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
+    requestAnimationFrame(doFrame);
 }
