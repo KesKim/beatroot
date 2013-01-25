895a2ad897fc0cce9f69d6f7cd4b338dbcc4d39c
Olli Etuaho <olli.etuaho@gmail.com>
2013-01-25 23:17:42 +0200

    Draw/update functions and time handling fleshed out

--- app.js
+++ app.js
@@ -1,20 +1,40 @@
 var canvas;
 var ctx;
 
+var time = {
+    current: 0,
+    lastFrame: 0,
+    start: 0
+};
+
+var game = {
+    funk: 0
+};
+
 (function() {
   var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
                               window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
   window.requestAnimationFrame = requestAnimationFrame;
 })();
 
-var start = Date.now(); 
-
 function doFrame() {
-    var timestamp = Date.now(); 
-    var progress = timestamp - start;
-    ctx.fillStyle = 'rgb(' + Math.round(Math.sin(progress * 0.001) * 128 + 128) + ',0, 0)';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    time.current = Date.now(); 
+    var timeDelta = time.current - time.lastFrame;
+    if (timeDelta > 10) { // don't bother with drawing frames that are too close to each other
+        time.lastFrame = time.current;
+        updateGame(timeDelta);
+        drawFrame();
+    }
     requestAnimationFrame(doFrame);
+}
+
+function updateGame(timeDelta) {
+    game.funk = Math.sin(time.current * 0.002) * 0.5 + 0.5;
+}
+
+function drawFrame() {
+    ctx.fillStyle = 'rgb(' + Math.round(game.funk * 255) + ', 0, 0)';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
 function init() {
@@ -26,5 +46,7 @@ function init() {
     document.body.appendChild(canvas);
     ctx.fillStyle = '#f9c';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
+    startTime = Date.now(); 
+    lastFrameTime = Date.now();
     requestAnimationFrame(doFrame);
 }
