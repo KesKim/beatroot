78a7d4fd74deac348c2b1360ba1fa9b5a8373efe
Kimmo Keskinen <keskinenkimmo@gmail.com>
2013-01-26 11:36:30 +0200

    (gamewank.js): Minimal gameplay exists. It just doesn't work. Because of a bug.

--- gamewank.js
+++ gamewank.js
@@ -4,8 +4,8 @@ var GameWank = function() {
     this.background = null;
     this.wankedAmount = 0;
     this.wankRequired = 100;
-    this.oneWayLimit = 15;
-    this.oneWayWank = 0;
+    this.lastDirection = 0;
+    this.oneDirectionAdded = 0;
 };
 
 GameWank.prototype.draw = function(canvas, ctx) {
@@ -15,12 +15,14 @@ GameWank.prototype.draw = function(canvas, ctx) {
 	var drawPosX = this.lastWankPosition ? this.lastWankPosition.x : (canvas.width / 2);
 	var drawPosY = this.lastWankPosition ? this.lastWankPosition.y : (canvas.height / 2);
 	this.background.draw(ctx, drawPosX - (this.background.width / 2), drawPosY - (this.background.height / 2));
+
+    ctx.fillStyle = 'red';
+    ctx.fillText(this.wankedAmount, 10, 10);
 };
 
 GameWank.prototype.update = function(timeDelta) {
 	if ( this.clickHeld === true )
 	{
-
 	}
 };
 
@@ -37,29 +39,32 @@ GameWank.prototype.mousemove = function(event) {
 		var xDistance = currentPos.x - this.lastWankPosition.x;
 		console.log('Latest distance traveled: ' + xDistance);
 
-		if ( this.oneWayLimit == null )
+		var directionChanged = !(xDistance < 0 && this.lastDirection < 0) || !(xDistance >= 0 && this.lastDirection >= 0);
+
+		if ( directionChanged === true )
 		{
-			if ( xDistance < 0 )
-				this.oneWayLimit = -15;
-			else
-				this.oneWayLimit = 15;
+            console.log('direction just changed');
+			// Reset one-way-wank limiter.
+			this.oneDirectionAdded = 0;
 		}
 
-		if ( xDistance < 0 )
+		if ( this.oneDirectionAdded <= this.oneDirectionLimit )
 		{
-			if ( this.oneWayWank >= this.oneWayLimit )
-			{
-				
-			}
+			this.wankedAmount += xDistance;
+			this.oneDirectionAdded += Math.abs(xDistance);
 		}
 
+		this.lastDirection = xDistance < 0 ? -1 : 1;
 		this.lastWankPosition = currentPos
+		console.log();
 	}
 };	
 
 GameWank.prototype.mouseup = function(event) {
 	this.clickHeld = false;
+    this.lastDirection = 0;
 	this.lastWankPosition = null;
+    this.oneDirectionAdded = 0;
 };
 
 GameWank.prototype.load = function() {
@@ -71,4 +76,7 @@ GameWank.prototype.isFinished = function() {
 		return true;
 	else
     	return false;
+};
+
+GameWank.prototype.cleanUp = function() {
 };
