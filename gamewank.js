142e268323724b12cb39fa4c6402a1215cbf744f
Kimmo Keskinen <keskinenkimmo@gmail.com>
2013-01-26 02:04:37 +0200

    (gamewank.js): Massive wankery, but it's still nothing to let others play with.

--- gamewank.js
+++ gamewank.js
@@ -1,25 +1,52 @@
 var GameWank = function() {
+	console.log('Initiating: GameWank. Shake mouse horizontally.');
     this.isAnExample = false;
     this.background = null;
+    this.wankedAmount = 0;
+    this.wankRequired = 100;
 };
 
 GameWank.prototype.draw = function(canvas, ctx) {
-	this.background.draw(ctx, (canvas.width / 2) - (this.background.width / 2), (canvas.height / 2) - (this.background.height / 2));
+	var drawPosX = this.lastWankPosition ? this.lastWankPosition.x : (canvas.width / 2);
+	var drawPosY = this.lastWankPosition ? this.lastWankPosition.y : (canvas.height / 2);
+	this.background.draw(ctx, drawPosX - (this.background.width / 2), drawPosY - (this.background.height / 2));
 };
 
 GameWank.prototype.update = function(timeDelta) {
+	if ( this.clickHeld === true )
+	{
+
+	}
 };
 
 GameWank.prototype.mousedown = function(event) {
 	this.clickHeld = true;
+	this.lastWankPosition = event.canvasCoords;
 };
 
 GameWank.prototype.mousemove = function(event) {
-};
+	if ( this.clickHeld === true )
+	{
+		//var currentPos = event.position
+		//currentPosx - this.lastWankPosition.x // x moved
+
+		//this.lastWankPosition = event.x, event.y
+		this.lastWankPosition = event.canvasCoords;
+	}
+};	
 
 GameWank.prototype.mouseup = function(event) {
+	this.clickHeld = false;
+	this.lastWankPosition = null;
 };
 
 GameWank.prototype.load = function() {
 	this.background = new Sprite('350x150.gif');
+};
+
+Game.prototype.isFinished = function() {
+	if ( this.wankedAmount >= this.wankRequired )
+		return true;
+	else
+    	return false;
 };
