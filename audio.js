7de4f7abedcb3c5115d02b00099890dd2cb2632a
Kimmo Keskinen <keskinenkimmo@gmail.com>
2013-01-26 13:11:10 +0200

    (audio.js): Takes OGG, should play OGG when loaded and play() is called.

--- audio.js
+++ audio.js
@@ -1,20 +1,43 @@
-var Audio = function(filenameA, filenameB) {
+var Audio = function(filenameOgg) {
 	this.loaded = false;
 
 	this.filenameA = filenameA;
 	this.filenameB = filenameB;
 	this.audio = document.createElement('audio');
 	console.log(this.audio);
-	
-	var sourceA = document.createElement('source');
-	sourceA.src = 'Assets/' + filenameA;
-	sourceA.loop = true;
+	// TODO add looping.
 
-	var sourceB = document.createElement('source');
-	sourceB.src = 'Assets/' + filenameB;
+    if ( filenameOgg )
+    {
+        var sourceOgg = document.createElement('source');
+        sourceOgg.src = 'Assets/' + filenameOgg;
+        this.audio.appendChild(sourceOgg);
+    }
+
+    /*if ( filenameB )
+    {
+        var sourceB = document.createElement('source');
+        sourceB.src = 'Assets/' + filenameB;
+        this.audio.appendChild(sourceB);    
+    }*/
 
 	var that = this;
-	this.img.onload = function() {
+
+	sourceOgg.onload = function() {
 		that.loaded = true;
+
+		if ( playWhenLoaded != null )
+		{
+			this.play();
+		}
+	}
+}
+
+Audio.prototype.play = function () {
+	if ( this.loaded ) {
+		this.audio.play();
+	}
+	else {
+		this.playWhenLoaded = true;
 	}
 }
