8c9a4d48aed78d2cb684c2aef458218d91a7547c
Kimmo Keskinen <keskinenkimmo@gmail.com>
2013-01-26 20:21:35 +0200

    (game.js): ADD some test code for selective Dialog and DialogLine/string functionality.

--- game.js
+++ game.js
@@ -2,16 +2,19 @@ var Game = function() {
     this.isAnExample = true;
     this.funk = 0;
     this.balls = null;
+    this.dialog = null;
 };
 
 Game.prototype.draw = function(canvas, ctx) {
     ctx.fillStyle = 'rgb(' + Math.round(this.funk * 255) + ', 0, 0)';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     this.balls.drawRotated(ctx, canvas.width * 0.5, canvas.height * 0.5, this.funk * 2.0, this.funk * 0.5 + 1.0);
+    this.dialog.draw(ctx, 100, 300);
 };
 
 Game.prototype.update = function(timeDelta) {
     this.funk = Math.sin(time.current * 0.002) * 0.5 + 0.5;
+    this.dialog.update(timeDelta);
 };
 
 Game.prototype.mousedown = function(event) {
@@ -33,3 +36,17 @@ Game.prototype.load = function() {
 Game.prototype.isFinished = function() {
     return false;
 };
+
+Game.prototype.cleanUp = function() {
+
+}
+
+Game.prototype.startGame = function() {
+	this.dialog = new Dialog(
+    	new DialogLine('YOLO'),
+    	'LOL',
+    	'k?'
+    );
+
+    this.dialog.start();
+}
