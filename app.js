1d8a5ede6aa4c5a31c369a65071c2984cebeb0da
Olli Etuaho <olli.etuaho@gmail.com>
2013-01-27 02:15:44 +0200

    Type street cred in a way that has street cred

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ var GameSeries = function() {
 
     this.games = [
         new GameDubstep(),
-        new GameWank('Street-credibility', 1000, rockDialog, ['jungle.ogg'], 'bg-rock.png', 'comb.png', new Vec2(400, 180), 0, 100),
+        new GameWank('Street cred', 1000, rockDialog, ['jungle.ogg'], 'bg-rock.png', 'comb.png', new Vec2(400, 180), 0, 100),
         new GameWank('Ignition', 5000, jungleDialog, ['jungle.ogg'], 'bg-jungle.png', 'firethingy.png', new Vec2(320, 160), 140, 0),
         new GameThrow(ancientThrowSfx, ancientHitSfx ,new Vec2(320, 160), ancientDialog, ancientMusic, 'Self Confidence', 'spear.png','bg-ancient.png','thrown.png','spearthrow.png','bird.png'),
         new Game()
